b9603f2511caa14391dcc53328b4dae74147d27e
developer_07 <otabekshavkatov956@gmail.com>
2024-06-24 19:55:06 +0500

    [add] header-end-responsive

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let headerIframe = document.getElementById("headerIframe");
+let headerIframe = document.getElementById("headerIframe");
+let salom = document.querySelector(".salom");
+headerIframe.addEventListener("load", function (e) {
+  let headerDocument = headerIframe.contentDocument;
 
-  headerIframe.addEventListener("load", function () {
-    let headerDocument =
-      headerIframe.contentDocument || headerIframe.contentWindow.document;
-    let selectElements = headerDocument.querySelectorAll(".selection");
+  let selectElements = headerDocument.querySelectorAll(".selection");
+  let bottom = headerDocument.querySelector(".bottom");
+  let middleText = headerDocument.querySelector(".middle-text");
 
-    selectElements.forEach((selectElement) => {
-      selectElement.addEventListener("change", function () {
-        let selectedValue = this.value;
-        document.getElementById("showPage").src = selectedValue;
-      });
+  let gamburger = headerDocument.querySelector(".gamburger");
+  let select = headerDocument.querySelector(".select-page");
+  selectElements.forEach((selectElement) => {
+    selectElement.addEventListener("change", function () {
+      let selectedValue = this.value;
+      document.getElementById("showPage").src = selectedValue;
     });
+  });
+  middleText.addEventListener("click", function () {
+    document.getElementById("showPage").src =
+      "/page/markaz_tarixi/muassasa_yangiliklari.html";
+  });
+  gamburger.addEventListener("click", function () {
+    headerIframe.classList.toggle("active-header");
+    bottom.classList.toggle("active-bottom");
+    select.classList.toggle("active-select");
   });
 });
