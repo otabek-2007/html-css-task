35c677e6f910fc9e37cfcefecacccbb3a4a1ce35
developer_07 <otabekshavkatov956@gmail.com>
2024-06-27 14:35:10 +0500

    [add] new pages

--- js/app.js
+++ js/app.js
@@ -1,27 +1,508 @@
-let headerIframe = document.getElementById("headerIframe");
-let salom = document.querySelector(".salom");
-headerIframe.addEventListener("load", function (e) {
-  let headerDocument = headerIframe.contentDocument;
+const menu = [
+  {
+    id: "1",
+    name: "Марказ ҳақида",
+    children: [
+      {
+        id: "1_1",
+        name: "Марказ тарихи",
+        src: "/page/markaz_tarixi/markaz_tarixi.html",
+      },
+      {
+        id: "1_2",
+        name: "Вазифа ва функциялари",
+        src: "/page/markaz_tarixi/vazifa_va_funksiyalar.html",
+      },
+      {
+        id: "1_3",
+        name: "Марказ тузилмаси",
+        src: "/page/markaz_tarixi/markaz_tuzilmasi.html",
+      },
+      {
+        id: "1_4",
+        name: "Раҳбарият",
+        src: "/page/markaz_tarixi/rahbariyat.html",
+      },
+      {
+        id: "1_5",
+        name: "Илмий-техник кенгаш",
+        src: "/page/markaz_tarixi/ilmiy_texnik_kengash.html",
+      },
+      {
+        id: "1_6",
+        name: "Марказ бўлимлари",
+        src: "/page/markaz_tarixi/markaz_bulimlari.html",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "Асосий фаолият",
+    children: [
+      {
+        id: "2_1",
+        name: "Ихтиро ва фойдали моделлар йўналиши",
+        src: "/page/asosiy_faoliyat/ixtiro_va_foydali_yonalishlar.html",
+      },
+      {
+        id: "2_2",
+        name: "Саноат намуналари, селекция ютуклари, географик кўрсаткичлар товар келиб чиқиш важой номлари йўналиши",
+        src: "/page/asosiy_faoliyat/sanoat_namunalari.html",
+      },
+      {
+        id: "2_3",
+        name: "Товар белгилари ва хизмат кўрсатиш белгилари йуналиши",
+        src: "/page/asosiy_faoliyat/tovar_belgilari.html",
+      },
+      {
+        id: "2_4",
+        name: "Муаллифлик ҳукуки объектларини депонентлаш ҳамда ЭҲМ учун дастур ва маълумотлар базалари йўналиши",
+        src: "/page/asosiy_faoliyat/mualliflik_xuquqi.html",
+      },
+      {
+        id: "2_5",
+        name: "ТИҚҚМлар фаолиятини мувофиклаштириш йўналиши",
+        src: "/page/asosiy_faoliyat/TIKKM_faoliyatlari.html",
+      },
+      {
+        id: "2_6",
+        name: "Интеллектуал мулк объектларини фуқаролик муомаласига киритиш ва тижоратлаштиришга кўмаклашиш йўналиши",
+        src: "/page/asosiy_faoliyat/intelektual_mulk.html",
+      },
+      {
+        id: "2_7",
+        name: "Халқаро ҳамкорлик йўналиши",
+        src: "/page/asosiy_faoliyat/xalqaro_xamkorlik.html",
+      },
+      {
+        id: "2_8",
+        name: "Иш фаолиятини ташкиллаштириш ва назорат қилиш йўналиши",
+        src: "/page/asosiy_faoliyat/ish_faoliyatini_tashkillashtirish.html",
+      },
+      {
+        id: "2_9",
+        name: "Дастурий ва техник таъминоти бўлими",
+        src: "/page/asosiy_faoliyat/dasturiy_va_texnik.html",
+      },
+      {
+        id: "2_10",
+        name: "Молия-иқтисод бўлими",
+        src: "/page/asosiy_faoliyat/moliya_iqtisod.html",
+      },
+      {
+        id: "2_11",
+        name: "Кадрлар бўйича мутахассис",
+        src: "/page/asosiy_faoliyat/kadrlar_boyicha.html",
+      },
+      {
+        id: "2_12",
+        name: "Бош юрисконсульт",
+        src: "/page/asosiy_faoliyat/bosh_yuristkonsult.html",
+      },
+      {
+        id: "2_12",
+        name: "Маъмурий-хўжалик ишлари бўйича бош маслаҳатчи",
+        src: "/page/asosiy_faoliyat/mamuriy_hujalik.html",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "Ҳужжатлар",
+    children: [
+      {
+        id: "3_1",
+        name: "Соҳага оид халқаро ҳужжатлар",
+        src: "/page/hujjatlar/sohaga_oid.html",
+      },
+      {
+        id: "3_2",
+        name: "Қонунлар",
+        src: "/page/hujjatlar/qonunlar.html",
+      },
+      {
+        id: "3_3",
+        name: "Ўзбекистон Республикаси Президентининг фармонлари ва қарорлари",
+        src: "/page/hujjatlar/farmonlar.html",
+      },
+      {
+        id: "3_4",
+        name: "Вазирлар Маҳкамасининг қарорлари",
+        src: "/page/hujjatlar/vazirlar_vaxkamasi.html",
+      },
+    ],
+  },
+  {
+    id: "4",
+    name: "Ҳамкорлик",
+    children: [
+      {
+        id: "4_1",
+        name: "ТИҚҚМ",
+        children: [
+          {
+            id: "4_1_1",
+            name: "ТИККМ лойиҳасининг хужжатларива ташкиламалиёти этиш",
+            src: "/page/hamkorlik/TIKKM_xujjatlar.html",
+          },
+          {
+            id: "4_1_2",
+            name: "ТИҚҚМни ташкил этиш амалиёти",
+            src: "/page/hamkorlik/TIKKM_tashkil_etish.html",
+          },
+          {
+            id: "4_1_3",
+            name: "ТИҚҚМлар рўйхати",
+            src: "/page/hamkorlik/TIKKM_xujjatlar.html",
+          },
+          {
+            id: "4_1_4",
+            name: "ТИҚҚМда семинарлар тармоқли ўтказиш режа жадвали",
+            src: "/page/hamkorlik/asosiy_faoliyat.html",
+          },
+          {
+            id: "4_1_5",
+            name: "БИМТ ҳорижий давлатлардаги ТИҚҚМлар тармоқлари",
+            src: "/page/hamkorlik",
+          },
+          {
+            id: "4_1_6",
+            name: "ТИҚҚМлар боғланиш учун маълумотлар",
+            src: "/page/hamkorlik",
+          },
+        ],
+      },
+      {
+        id: "4_2",
+        name: "Халқаро хамкорлик",
+        src: "/page/hamkorlik/TIKKM_xujjatlar.html",
+      },
+      { id: "4_3", name: "Кўмаклашиш" },
+    ],
+  },
+  {
+    id: "5",
+    name: "Марказ хизматлари",
+    children: [
+      {
+        id: "5_1",
+        name: "Ихтиро ва фойдали моделлар бўйича",
+      },
+      {
+        id: "5_2",
+        name: "Саноат намунаси, географик кўрсаткичлар, товар келиб чиққан жой номи буйича",
+      },
+      {
+        id: "5_3",
+        name: "Товар белгиси ва кўрсатиш хизмат белгилари бўйича",
+      },
+      {
+        id: "5_4",
+        name: "Муаллифлик хукуки объектларини депонентлаш, ЭҲМ учун дастур ва маълумотлар бўйича",
+      },
+      {
+        id: "5_5",
+        name: "Ўкув курслари буйича",
+      },
+    ],
+  },
+  {
+    id: "6",
+    name: "Интерактив хизматлар",
+    children: [
+      {
+        id: "6_1",
+        name: "МХО депонентлаш учун аризани тўлдириш",
+      },
+      {
+        id: "6_2",
+        name: "Депозитарийлар томонидан муаллифлик хукуки объектлари электрон нусхаларини ягона базага киритиш",
+      },
+    ],
+  },
+  {
+    id: "7",
+    name: "Очиқ маълумотлар",
+    children: [
+      {
+        id: "7_1",
+        name: "Нашрлар",
+        children: [
+          {
+            id: "7_1_1",
+            name: "Расмий ахборотнома",
+          },
+          {
+            id: "7_1_2",
+            name: "Йиллик курсаткичлар",
+          },
+          {
+            id: "7_1_3",
+            name: "Йиллик ҳисобот",
+          },
+          {
+            id: "7_1_4",
+            name: "Услубий қўлламалар",
+          },
+        ],
+      },
+      {
+        id: "7_2",
+        name: "Саноат мулки объектлари буйича статистик маълумотлар динамикаси",
+        children: [
+          {
+            id: "7_2_1",
+            name: "Ихтиро ва фойдали моделлар",
+          },
+          {
+            id: "7_2_2",
+            name: "Саноат намунаси",
+          },
+          {
+            id: "7_2_3",
+            name: "Селекция ютуклари",
+          },
+          {
+            id: "7_2_4",
+            name: "Товар белгилари ва хизмат кўрсатиш белгилари",
+          },
+          {
+            id: "7_2_5",
+            name: "Географик кўрсаткичлар",
+          },
+          {
+            id: "7_2_6",
+            name: "Товар келиб чиққан жой номи",
+          },
+        ],
+      },
+      {
+        id: "7_3",
+        name: "Муаллифлик ҳуқуқи объектлари бўйича  статистик маълумотлар динамикаси",
+        children: [
+          {
+            id: "7_3_1",
+            name: "Депонентланган адабиёт, фан ва санъат асарлари",
+          },
+          {
+            id: "7_3_2",
+            name: "ЭХМ дастурлар ва маълумотлар базалари",
+          },
+        ],
+      },
+      {
+        id: "7_4",
+        name: "Патент вакиллари рўйҳати",
+      },
+      {
+        id: "7_5",
+        name: "Коррупцияга қарши кураш",
+      },
+    ],
+  },
+  {
+    id: "8",
+    name: "Медиатека",
+    children: [
+      {
+        id: "8_1",
+        name: "Марказ янгиликлари",
+      },
+      {
+        id: "8_2",
+        name: "Пресс релизлар",
+      },
+      {
+        id: "8_3",
+        name: "Фотогаларея",
+      },
+      {
+        id: "8_4",
+        name: "Видеогаларея",
+      },
+    ],
+  },
+  {
+    id: "9",
+    name: "Боғланиш",
+    children: [
+      {
+        id: "9_1",
+        name: "Контактлар",
+      },
+      {
+        id: "9_2",
+        name: "Барча рақамлар",
+      },
+      {
+        id: "9_3",
+        name: "Фукаролар мурожаати",
+      },
+      {
+        id: "9_4",
+        name: "Куп бериладиган саволлар",
+      },
+      {
+        id: "9_5",
+        name: "Банк реквизитлар",
+      },
+    ],
+  },
+];
 
-  let selectElements = headerDocument.querySelectorAll(".selection");
-  let bottom = headerDocument.querySelector(".bottom");
-  let middleText = headerDocument.querySelector(".middle-text");
+function createMenuItems(menuItems, parentUl) {
+  try {
+    menuItems.forEach((menuItem, index) => {
+      const li = document.createElement("li");
+      const div = document.createElement("div");
+      div.className = "li-content";
 
-  let gamburger = headerDocument.querySelector(".gamburger");
-  let select = headerDocument.querySelector(".select-page");
-  selectElements.forEach((selectElement) => {
-    selectElement.addEventListener("change", function () {
-      let selectedValue = this.value;
-      document.getElementById("showPage").src = selectedValue;
+      const img = document.createElement("img");
+      img.src =
+        menuItem.children && menuItem.children.length > 0
+          ? "/image/drop_down.png"
+          : "";
+      img.className = "menu-icon";
+
+      const span = document.createElement("span");
+      span.textContent = menuItem.name;
+
+      div.appendChild(span);
+      div.appendChild(img);
+      li.appendChild(div);
+      parentUl.appendChild(li);
+
+      li.addEventListener("click", (event) => {
+        event.stopPropagation();
+        if (menuItem.src) {
+          document.getElementById("showPage").src = menuItem.src;
+        }
+      });
+
+      if (menuItem.children && menuItem.children.length > 0) {
+        const popupDiv = document.createElement("div");
+        popupDiv.className = "popup-li-content";
+        popupDiv.style.display = "none";
+
+        const childUl = document.createElement("ul");
+        childUl.className = "child-ul-content";
+        // childUl.setAttribute('src',menuItem.src)
+
+        createMenuChildItems(menuItem.children, childUl, 0);
+        popupDiv.appendChild(childUl);
+        div.appendChild(popupDiv);
+
+        div.addEventListener("click", (event) => {
+          event.stopPropagation();
+          const isPopupVisible = popupDiv.style.display === "block";
+          document.querySelectorAll(".popup-li-content").forEach((popup) => {
+            popup.style.display = "none";
+          });
+          popupDiv.style.display = isPopupVisible ? "none" : "block";
+        });
+      }
     });
-  });
-  middleText.addEventListener("click", function () {
-    document.getElementById("showPage").src =
-      "/page/markaz_tarixi/muassasa_yangiliklari.html";
-  });
+  } catch (error) {
+    console.error("Error creating menu items:", error);
+  }
+}
+
+function createMenuChildItems(menuItems, parentUl, level) {
+  try {
+    menuItems.forEach((menuItem, index) => {
+      const li = document.createElement("li");
+
+      li.setAttribute("src", menuItem.src);
+
+      const div = document.createElement("div");
+      const menu_line = document.createElement("div");
+      const menu_line_in = document.createElement("div");
+      menu_line.className = "menu-active-line";
+      menu_line_in.className = "menu-active-line-in";
+
+      const img = document.createElement("img");
+      img.src =
+        menuItem.children && menuItem.children.length > 0
+          ? "/image/drop_down_right.png"
+          : "";
+      img.className = "child-menu-icon";
+      div.className = "child-content-box";
+
+      const span = document.createElement("span");
+      span.textContent = menuItem.name;
+
+      div.appendChild(span);
+      div.appendChild(img);
+      li.appendChild(div);
+      menu_line.appendChild(menu_line_in);
+      li.appendChild(menu_line);
+      parentUl.appendChild(li);
+
+      if (menuItem.children && menuItem.children.length > 0) {
+        const childUl = document.createElement("ul");
+        childUl.className = "child-ul-content-in";
+        childUl.style.display = "none";
+
+        createMenuChildItems(menuItem.children, childUl, level + 1);
+        li.appendChild(childUl);
+
+        div.addEventListener("click", (event) => {
+          event.stopPropagation();
+          const isPopupVisible = childUl.style.display === "block";
+          document.querySelectorAll(".child-ul-content-in").forEach((popup) => {
+            popup.style.display = "none";
+          });
+          childUl.style.display = isPopupVisible ? "none" : "block";
+          childUl.style.top = `${index * 50}px`;
+        });
+      }
+    });
+  } catch (error) {
+    console.error("Error creating menu child items:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuUl = document.querySelector(".menu-ul");
+  const gamburger = document.querySelector(".gamburger");
   gamburger.addEventListener("click", function () {
-    headerIframe.classList.toggle("active-header");
-    bottom.classList.toggle("active-bottom");
-    select.classList.toggle("active-select");
+    menuUl.classList.toggle("active");
   });
 });
+// Call the function to create the menu
+document.addEventListener("DOMContentLoaded", () => {
+  const menuContainer = document.getElementById("menu-container");
+  createMenuItems(menu, menuContainer);
+
+  const elements = document.querySelectorAll(".child-ul-content li");
+  elements.forEach((e) => {
+    e.addEventListener("click", function (event) {
+      const src = event.currentTarget.getAttribute("src");
+      if (src) {
+        loadIframe(src);
+      } else {
+        // Set up event listeners for child elements if src is undefined
+        const elements_child = event.currentTarget.querySelectorAll(
+          ".child-ul-content-in li"
+        );
+        elements_child.forEach((childElement) => {
+          childElement.addEventListener("click", function (event) {
+            const childSrc = event.currentTarget.getAttribute("src");
+            if (childSrc) {
+              loadIframe(childSrc);
+            }
+          });
+        });
+      }
+    });
+  });
+});
+
+function loadIframe(src) {
+  const iframe = document.getElementById("iframe-id"); // Change this to your actual iframe ID
+  iframe.src = src;
+}
+
+function loadIframe(src) {
+  document.getElementById("showPage").src = src;
+}
